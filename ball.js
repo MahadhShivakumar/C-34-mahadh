c5b171e3f637bb97cffb7dac35589ee7e6570a1b
unknown <maduexplosive@gmail.com>
2021-01-26 14:59:05 +0530

    a

--- ball.js
+++ ball.js
@@ -1,25 +1,24 @@
-class Box {
-    constructor(x,y,width,height) {
+class Ball {
+    constructor(x,y,width,height,angle) {
       var options = {
-        'restitution':0.1,
-        'friction':0.8,
-        'density':0.04
+        'frictionAir':2,
+        'density':8
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
     }
-    display(){
-        var pos =this.body.position;                                        
+    display(){                                      
         var angle =this.body.angle;
+        var pos =this.body.position;                                        
         push();
         stroke("red")
         strokeWeight(2)
         translate(pos.x,pos.y)
         rotate(angle)
-        rectMode(CENTER)
-        rect(0,0, this.width, this.height);
+        ellipseMode(RADIUS)
+        ellipse(0,0, this.width, this.height);
         pop()  
     }
   };
